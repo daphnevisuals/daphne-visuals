9c224e6de03b3cf5c2e1277f4554a47409121c8d
Daphne Staruch <daphstr@gmail.com>
2023-09-23 19:23:36 -0400

    added all swipers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,26 +9,47 @@ class PagesController < ApplicationController
   end
 
   def photography
+
+    # clubbing
     @images_clubbing = Cloudinary::Api.resources(
       type: 'upload',
-      prefix: 'Photography/Clubbing/'
+      prefix: 'Photography/Clubbing/',
+      max_results: 1000
     )
+    @images_clubbing['resources'].shuffle!
+
+
+    # mariages
     @images_mariages = Cloudinary::Api.resources(
       type: 'upload',
-      prefix: 'Photography/Mariages/'
+      prefix: 'Photography/Mariages/',
+      max_results: 1000
     )
+    @images_mariages['resources'].shuffle!
+
+    # animaux
     @images_animaux = Cloudinary::Api.resources(
       type: 'upload',
-      prefix: 'Photography/Mariages/'
+      prefix: 'Photography/Animaux/',
+      max_results: 1000
     )
+    @images_animaux['resources'].shuffle!
+
+    # portraits
     @images_portraits = Cloudinary::Api.resources(
       type: 'upload',
-      prefix: 'Photography/Mariages/'
+      prefix: 'Photography/Portraits/',
+      max_results: 1000
     )
+    @images_portraits['resources'].shuffle!
+
+    # street
     @images_street = Cloudinary::Api.resources(
       type: 'upload',
-      prefix: 'Photography/Mariages/'
+      prefix: 'Photography/Street/',
+      max_results: 1000
     )
+    @images_street['resources'].shuffle!
   end
 
   def webdesign
