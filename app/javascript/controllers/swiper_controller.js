0f4b97967f8aa21a9182aa521d7ae26cb79dadb5
Daphne Staruch <daphstr@gmail.com>
2023-09-22 23:56:09 -0400

    trying to make swiper work

---
+++ app/javascript/controllers/swiper_controller.js
@@ -0,0 +1,38 @@
+// import { Controller } from "@hotwired/stimulus";
+// import Swiper, { Navigation, Pagination, Autoplay, EffectFade } from "swiper";
+
+// Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
+// // Connects to data-controller="swiper"
+// export default class extends Controller {
+//   connect() {
+//     // swiper
+
+//     var swiper = new Swiper(".mySwiper", {
+//       observer: true,
+//       observeParents: true,
+//       parallax: true,
+//       loop: true,
+//       direction: "horizontal",
+//       slidesPerView: 1,
+//       effect: "fade",
+//       lazy: true,
+//       loop: true,
+//       speed: 1500,
+//       autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: true,
+//       },
+//       fadeEffect: {
+//         crossFade: true,
+//       },
+//       navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       },
+//       pagination: {
+//         el: ".swiper-pagination",
+//         type: "fraction",
+//       },
+//     });
+//   }
+// }
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "gsap", to: "https://ga.jspm.io/npm:gsap@3.12.2/index.js"
+pin "swiper/element/bundle", to: "https://ga.jspm.io/npm:swiper@10.3.0/swiper-element-bundle.mjs"
