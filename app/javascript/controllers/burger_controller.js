e350b8117ec7c2c4369fff349a0d2be0045a1398
Daphne Staruch <daphstr@gmail.com>
2023-10-19 13:32:05 +0200

    button animation

--- app/javascript/controllers/burger_controller.js
+++ app/javascript/controllers/burger_controller.js
@@ -2,10 +2,13 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="burger"
 export default class extends Controller {
-  static targets = ["toggleMenu"];
+  static targets = ["toggleMenu", "burgerAnimation"];
 
-  connect() {}
+  connect() {
+    console.log(this.burgerAnimationTarget);
+  }
   menu() {
     this.toggleMenuTarget.classList.toggle("d-none");
+    this.burgerAnimationTarget.classList.toggle("animate");
   }
 }
